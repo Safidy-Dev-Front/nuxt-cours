75bafdef67713f9cb9fa9e42a3aa67320f8d2b53
Safidi <safidiniainaromino13@gmail.com>
2023-09-20 08:43:10 +0300

    feat: first commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>This is the main page</div>
+    <div>
+      <p>Liste user</p>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          <p>{{user.name}}</p>
+          <p>{{user.username}}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup>
+  const users = ref([])
+   asyncData(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then( data => users.value = data)
+  })
+</script>
